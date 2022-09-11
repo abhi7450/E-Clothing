d73e28ce2a8b85a7d2b5acc2ab0e871d1a6ee849
Abhishek Ashok <abhishekashok64@gmail.com>
2022-09-11 16:17:29 +0530

    added in our addToCart Feature and the CartItem design

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cart.context";
 import Button from "../button/button.component";
+import CartItem from "../cart-item/cart-item.component";
 import "./cart-dropdown.styles.scss";
 const CardDropdown = () => {
+    const { cartItems } = useContext(CartContext);
     return (
         <div className="cart-dropdown-container">
-            <div className="cart-items" />
+            <div className="cart-items">
+                {cartItems.length ? (
+                    cartItems.map((item) => <CartItem key={item.id} cartItem={item} />)
+                ) : (
+                    <span className="empty-message">Your cart is empty</span>
+                )}
+            </div>
             <Button>GO TO CHECKOUT</Button>
         </div>
     );
